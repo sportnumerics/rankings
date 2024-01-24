3e547fa26f821d49f59f096d42b4d312bab93e20
Will James <jameswt@gmail.com>
2024-01-23 20:56:35 -0600

    Allow triggering specific years via container override

--- backend/run.sh
+++ backend/run.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+if [ ! -z "$1" ]; then
+  container_override='{"name":"rankings-backend", "environment":[{"name":"YEAR", "value":"'"$1"'"}]}'
+else
+  container_override=''
+fi
+
 aws ecs run-task \
   --task-definition arn:aws:ecs:us-west-2:265978616089:task-definition/rankings-backend-dev:3 \
   --cluster rankings-backend-dev \
   --launch-type FARGATE \
-  --network-configuration "awsvpcConfiguration={subnets=[subnet-f728fa92,subnet-866f9ff1],assignPublicIp=ENABLED}"
+  --network-configuration "awsvpcConfiguration={subnets=[subnet-f728fa92,subnet-866f9ff1],assignPublicIp=ENABLED}" \
+  --overrides '{"containerOverrides":['"$container_override"']}'
