f8cd99d9b211ea7ddbd54c0820e43dd68526b8a2
Will James <jameswt@gmail.com>
2024-12-04 22:05:50 -0600

    Add PutRolePermissionsBoundary

--- infrastructure/modules/cicd/main.tf
+++ infrastructure/modules/cicd/main.tf
@@ -61,7 +61,8 @@ resource "aws_iam_policy" "deployment_role" {
         Action = [
           "iam:CreateRole",
           "iam:AttachRolePolicy",
-          "iam:DetachRolePolicy"
+          "iam:DetachRolePolicy",
+          "iam:PutRolePermissionsBoundary"
         ],
         Resource = [
           "arn:aws:iam::265978616089:role/rankings-*"
